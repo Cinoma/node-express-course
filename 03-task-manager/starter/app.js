62dd6aa21bcaedbec2f85b2c762e82760a4d0763
Chris Inoma <84427950+Cinoma@users.noreply.github.com>
2021-11-13 18:03:56 -0600

    Rearange folders

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
