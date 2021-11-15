abcecc0f6e328bf4a4eb254824f127b25b0d009b
Chris Inoma <84427950+Cinoma@users.noreply.github.com>
2021-11-14 19:15:52 -0600

    Exercise-03: Built Create(CRUD) func+Mongo 1:42:45

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 
@@ -15,4 +17,13 @@ app.use("/api/v1/tasks", tasks);
 
 const PORT = 3000;
 
-app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
