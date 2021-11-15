9e932eeae7761070ecdc97a6fad9f439b2129fd1
Chris Inoma <84427950+Cinoma@users.noreply.github.com>
2021-11-14 19:06:24 -0600

    Worked on Exercise-03 and built Create(CRUD) func

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,15 @@
-const connectionString =
-  "mongodb+srv://MainUser:<password>@cluster0.emlnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+// .then(() => console.log("CONNECTED TO THE DB..."))
+// .catch((err) => console.log(err));
+
+module.exports = connectDB;
