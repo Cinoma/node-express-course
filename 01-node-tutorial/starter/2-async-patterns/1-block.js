cb7aafaae14800261d867ba642be35c5c82e8448
Chris Inoma <84427950+Cinoma@users.noreply.github.com>
2021-11-27 23:51:55 -0600

    Worked optional exercises 2:55:40

---
+++ 01-node-tutorial/starter/2-async-patterns/1-block.js
@@ -0,0 +1,21 @@
+const { createServer } = require("http");
+
+const server = createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+  }
+  if (req.url === "/about") {
+    // Blocking Code for example
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("About Page");
+  }
+  res.end("Error Page");
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
